f71c1a085ba7ccf78e09602bde0b08ebae3c7ecc
woobin <guiyomyee@gmail.com>
2026-05-21 22:30:33 +0900

    ãtensor storage

--- ex.py
+++ ex.py
@@ -1,15 +1,23 @@
 import torch
+import numpy
 
-#1차 텐서
-a = torch.ones(3)
-print(f"1D 텐서: {a}, Shape: {a.shape}")
+# #1차 텐서
+# a = torch.ones(3)
+# print(f"1D 텐서: {a}, Shape: {a.shape}")
+#
+# #2차원 텐서
+# points = torch.tensor([[4.0,1.0], [5.0,3.0],[2.0,1.0]])
+# print(f"2D 텐서:\n{points}, Shape: {points.shape}")
+#
+# #3차원 텐서
+# img_t = torch.randn(3,5,5)
+# print(f"3D 텐서 (이미지): Shape: {img_t.shape}")
+#
+#
 
-#2차원 텐서
-points = torch.tensor([[4.0,1.0], [5.0,3.0],[2.0,1.0]])
-print(f"2D 텐서:\n{points}, Shape: {points.shape}")
+points = torch.tensor([[4.0,1.0], [5.0,3.0], [2.0,1.0]])
 
-#3차원 텐서
-img_t = torch.randn(3,5,5)
-print(f"3D 텐서 (이미지): Shape: {img_t.shape}")
-
-
+print(points.storage())
+print(points.storage_offset())
+print(points.size())
+print(points.stride())
